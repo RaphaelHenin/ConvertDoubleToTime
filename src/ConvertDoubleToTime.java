7952f6f76c15e88b1c5ee309fd96a02c99a12b51
Raphael Henin <heniin.raphael@gmail.com>
2019-01-14 16:55:49 +0100

    Adding a method that manage the display of the time format

--- src/ConvertDoubleToTime.java
+++ src/ConvertDoubleToTime.java
@@ -4,40 +4,72 @@ import java.math.RoundingMode;
 
 public class ConvertDoubleToTime {
 
-	static String[] convertDoubleToTime(String time, String[] temp, int i) {
-		String[] numbers = time.split("\\.");
-		Double value = Double.parseDouble(time);
+	/**
+	 * 
+	 * @param time
+	 *            = the double value that we wanna convert to time format
+	 * @param temp
+	 *            = Table string that will store the time
+	 * @param i
+	 * @return temp, the string table that have stored the time through all
+	 *         recursive
+	 */
+	static String[] convertDoubleToTime(Double time, String[] temp, int i) {
+		String stringTime = time.toString();
+		String[] numbers = stringTime.split("\\.");
 		int firstUnit = Integer.parseInt(numbers[0]);
-		if (value > 60) {
-			value = value / 60;
-			String hourUnit = (Double.toString(value)).split("\\.")[0];
+		if (time > 60) {
+			time = time / 60;
+			String hourUnit = (Double.toString(time)).split("\\.")[0];
 			temp[i] = hourUnit;
-			Double newValue = value - Double.parseDouble(hourUnit);
+			Double newValue = time - Double.parseDouble(hourUnit);
 			if (newValue == 0)
 				return temp;
 			BigDecimal bd = new BigDecimal(newValue * 60);
-		    bd = bd.setScale(2, RoundingMode.HALF_UP);
-			temp = convertDoubleToTime(bd.toString(), temp, i);
+			bd = bd.setScale(2, RoundingMode.HALF_UP);
+			temp = convertDoubleToTime(bd.doubleValue(), temp, i);
 		} else {
 			temp[i + 1] = Integer.toString(firstUnit);
-			Double newValue = value - firstUnit;
+			Double newValue = time - firstUnit;
 			if (newValue == 0)
 				return temp;
 			BigDecimal bd = new BigDecimal(newValue * 60);
-		    bd = bd.setScale(2, RoundingMode.HALF_UP);
-			temp = convertDoubleToTime(bd.toString(), temp, i + 1);
+			bd = bd.setScale(2, RoundingMode.HALF_UP);
+			temp = convertDoubleToTime(bd.doubleValue(), temp, i + 1);
 		}
-		for(int y=0;y<temp.length;y++)
-			if(temp[y]==null)
-				temp[y] = "00";
 		return temp;
+	}
+
+	/**
+	 * @param String
+	 *            table that stored the time calculated in the
+	 *            convertDoubelToTime function
+	 * @return "HH:MM:SS" time format (if HH, MM or SS are not null)
+	 */
+	static String displayTime(String[] time) {
+		String timeToDisplay = "";
+		// If hour, display it
+		if (time[0] != null) {
+			timeToDisplay += time[0] + ":";
+		}
+		// If minute, display it
+		if (time[1] != null) {
+			// If second, display it ELSE just diplay minute whithout ":" after.
+			if (time[2] != null) {
+				timeToDisplay += time[1] + ":" + time[2];
+			} else {
+				timeToDisplay += time[1];
+			}
+		}
+		// milliseconds are not displayed
+		return timeToDisplay;
 	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		String[] time = new String[4];
-		time = convertDoubleToTime("11.98", time, 0);
-		System.out.println(time[0]+"h"+time[1]+"m"+time[2]+"s"+time[3]);
+		time = convertDoubleToTime(111.0, time, 0);
+		System.out.println(displayTime(time));
 	}
 
 }
