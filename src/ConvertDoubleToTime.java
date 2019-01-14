f0d4cf2e4190455b646213f52ad34c58b954cc4f
Raphael Henin <heniin.raphael@gmail.com>
2019-01-14 17:37:00 +0100

    Change String table to an arraylist of String to avoid teh use of index (param of the old method)

--- src/ConvertDoubleToTime.java
+++ src/ConvertDoubleToTime.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 
 public class ConvertDoubleToTime {
 
@@ -14,28 +15,30 @@ public class ConvertDoubleToTime {
 	 * @return temp, the string table that have stored the time through all
 	 *         recursive
 	 */
-	static String[] convertDoubleToTime(Double time, String[] temp, int i) {
+	static ArrayList<String> convertDoubleToTime(Double time, ArrayList<String> temp) {
 		String stringTime = time.toString();
 		String[] numbers = stringTime.split("\\.");
 		int firstUnit = Integer.parseInt(numbers[0]);
-		if (time > 60) {
+		if (time >= 60) {
 			time = time / 60;
 			String hourUnit = (Double.toString(time)).split("\\.")[0];
-			temp[i] = hourUnit;
+			temp.add(hourUnit);
 			Double newValue = time - Double.parseDouble(hourUnit);
 			if (newValue == 0)
 				return temp;
 			BigDecimal bd = new BigDecimal(newValue * 60);
 			bd = bd.setScale(2, RoundingMode.HALF_UP);
-			temp = convertDoubleToTime(bd.doubleValue(), temp, i);
+			temp = convertDoubleToTime(bd.doubleValue(), temp);
 		} else {
-			temp[i + 1] = Integer.toString(firstUnit);
+			if (temp.size() < 1)
+				temp.add(0, null);
+			temp.add(Integer.toString(firstUnit));
 			Double newValue = time - firstUnit;
 			if (newValue == 0)
 				return temp;
 			BigDecimal bd = new BigDecimal(newValue * 60);
 			bd = bd.setScale(2, RoundingMode.HALF_UP);
-			temp = convertDoubleToTime(bd.doubleValue(), temp, i + 1);
+			temp = convertDoubleToTime(bd.doubleValue(), temp);
 		}
 		return temp;
 	}
@@ -46,29 +49,40 @@ public class ConvertDoubleToTime {
 	 *            convertDoubelToTime function
 	 * @return "HH:MM:SS" time format (if HH, MM or SS are not null)
 	 */
-	static String displayTime(String[] time) {
+	static String displayTime(ArrayList<String> time) {
 		String timeToDisplay = "";
 		// If hour, display it
-		if (time[0] != null) {
-			timeToDisplay += time[0] + ":";
-		}
-		// If minute, display it
-		if (time[1] != null) {
-			// If second, display it ELSE just diplay minute whithout ":" after.
-			if (time[2] != null) {
-				timeToDisplay += time[1] + ":" + time[2];
-			} else {
-				timeToDisplay += time[1];
-			}
-		}
+		if (time.get(0) != null)
+			// If minute, display it
+			if (time.size() > 1)
+				// If second, display it ELSE just display minute without ":"
+				// after.
+				if (time.size() > 2)
+					timeToDisplay += time.get(0) + ":" + time.get(1) + ":" + time.get(2) + " heures";
+				else
+					timeToDisplay += time.get(0) + ":" + time.get(1) + "  heures";
+
+			else
+				timeToDisplay += time.get(0) + " heures";
+
+		else 
+			if (time.get(1) != null)
+				if (time.size() > 2)
+					timeToDisplay += time.get(1) + ":" + time.get(2) + " minutes";
+				else
+					timeToDisplay += time.get(1) + " minute(s)";
+			else 
+				if (time.get(2) != null)
+					timeToDisplay += time.get(2) + " seconde(s)";
+
 		// milliseconds are not displayed
 		return timeToDisplay;
 	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		String[] time = new String[4];
-		time = convertDoubleToTime(111.0, time, 0);
+		ArrayList<String> time = new ArrayList<>();
+		time = convertDoubleToTime(0.1, time);
 		System.out.println(displayTime(time));
 	}
 
